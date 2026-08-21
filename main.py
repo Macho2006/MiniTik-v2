777ab59f92a68ee846d1425d768ec85c1b25cb7c
Macho2006 <mocugo2006@gmail.com>
2026-08-20 18:19:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ if is_prod:
     app.config['REMEMBER_COOKIE_SECURE'] = True
     app.config['SESSION_COOKIE_SAMESITE'] = 'None'
     app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['SESSION_COOKIE_DOMAIN'] = '.onrender.com'
 else:
     app.config['REMEMBER_COOKIE_NAME'] = 'minitik_remember_v3' # BUMPED TO V3
     app.config['SESSION_COOKIE_NAME'] = 'minitik_session_v3' # BUMPED TO V3
